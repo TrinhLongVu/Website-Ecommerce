880d24bf828c9f36be8b09e581ed6d08d259c2e9
NhanIsCoding <thanhthiennhan2003@gmail.com>
2023-12-16 10:23:52 +0700

    Front-end Authentication Pages - version 1

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 
+import Authentication from "./pages/Authentication/Authentication";
+
 import ScrollTop from "./components/ScrollTop/ScrollTop";
 
 import Main from "./layouts/Main";
@@ -12,6 +14,16 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />}>
           <Route index element={<Home />} />
+        </Route>
+        <Route path="/authentication">
+          <Route
+            path="/authentication/login"
+            element={<Authentication authenType={"Login"} />}
+          />
+          <Route
+            path="/authentication/register"
+            element={<Authentication authenType={"Register"} />}
+          />
         </Route>
       </Routes>
     </>
--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -14,7 +14,8 @@ const Home = () => {
   const product = {
     id: "123",
     name: "Celestial Glow Crystal Pendant",
-    image: "https://example.com/images/products/shoes/red-high-heels.jpg",
+    image:
+      "https://images.unsplash.com/photo-1552566626-52f8b828add9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
     desc: "Illuminate your style with the ethereal beauty of our Celestial Glow Crystal Pendant. This exquisite piece features a radiant crystal encased in a sterling silver setting, capturing the essence of starlight in a timeless design. The pendant exudes a captivating glow that adds a touch of celestial elegance to any outfit, making it the perfect accessory for both casual and formal occasions.",
     price: "$89.99",
     time: "2 hours ago",
