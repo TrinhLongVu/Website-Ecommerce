0005402033f12d0e35d9b9d5f59779c56bbe416b
NhanIsCoding <thanhthiennhan2003@gmail.com>
2023-12-16 09:57:14 +0700

    Front-end Footer Component - version 1 & Main Layout - version 2

---
+++ front-end/src/layouts/Footer/Footer.jsx
@@ -0,0 +1,74 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faEarth,
+  faTags,
+  faCircleInfo,
+  faNewspaper,
+} from "@fortawesome/free-solid-svg-icons";
+import "./footer.css";
+
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  const today = new Date();
+
+  const categoryList = [
+    {
+      name: "Electronics",
+      link: "/categories/electronics",
+    },
+    {
+      name: "Furniture",
+      link: "/categories/furniture",
+    },
+  ];
+
+  return (
+    <footer>
+      <div className="foot-content">
+        <div className="foot-row">
+          <div className="foot-left-section">
+            <div className="foot-title">
+              <div>
+                <FontAwesomeIcon icon={faCircleInfo} className="ico" /> The Mega
+                Mall
+              </div>
+            </div>
+            <div className="foot-section-content" id="foot-mega-para">
+              Welcome to The Mega Mall, where luxury meets convenience in the
+              world of online shopping. At The Mega Mall, we curate a
+              sophisticated collection of the finest products, ensuring that
+              each item reflects the epitome of elegance and quality. Dive into
+              a seamless shopping experience that transcends the ordinary, with
+              a user-friendly interface designed to make your online journey
+              delightful and effortless.
+            </div>
+          </div>
+          <div className="foot-right-section">
+            <div className="foot-title">
+              <div>
+                <FontAwesomeIcon icon={faTags} className="ico" /> Categories
+              </div>
+            </div>
+            <div className="foot-section-content" id="foot-categories">
+              {categoryList.map((category, idx) => (
+                <Link to={category.link} key={idx}>
+                  {category.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="foot-banner">
+        <div>
+          <a href="/policy">Privacy Policy | Terms & Conditions</a>
+        </div>
+        <div>Copyright © The Mega Mall @ {today.getFullYear()}</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- front-end/src/layouts/Main.jsx
+++ front-end/src/layouts/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Header from "../components/Header/Header";
+import Header from "./Header/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer/Footer";
 const MainLayout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
