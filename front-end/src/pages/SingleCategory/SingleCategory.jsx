b759c35cf2c7006a92a4c7933d4bd09e8615fbac
SimonGin <tt26419@gmail.com>
2024-01-12 10:58:41 +0700

    Front-end Fix Filter For SingleCategory Page

--- front-end/src/pages/SingleCategory/SingleCategory.jsx
+++ front-end/src/pages/SingleCategory/SingleCategory.jsx
@@ -2,64 +2,36 @@ import { useOutletContext, useParams } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
 // Assets
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTag, faXmark, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { faTag } from "@fortawesome/free-solid-svg-icons";
 import categoryImg from "../../assets/category_bg.jpeg";
 // Components
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import ProductShelf from "../../components/ProductShelf/ProductShelf";
 import Pagination from "../../components/Pagination/Pagination";
 import Loader from "../../components/Loader/Loader";
+import Filter from "../../components/Filter/Filter";
 // Style
 import "./single-category.css";
 // Implementation
 const SingleCategory = () => {
+  const domain =
+    "https://themegamall.onrender.com/api/v1/category/page?limit=8&";
   const { name } = useParams();
   const prevNameRef = useRef("");
   const prevFilter = useRef("all");
   const { categoryList } = useOutletContext();
-  const domain =
-    "https://themegamall.onrender.com/api/v1/category/page?limit=8&";
   const pageCategory = categoryList.find((category) => category.name === name);
   const [loadPage, setLoadPage] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [productList, setProductList] = useState([]);
-  const [showFilter, setShowFilter] = useState(false);
   const [filter, setFilter] = useState("");
   useEffect(() => {
     setCurrentPage(1);
     setFilter("");
   }, [name]);
 
-  const toggleFilter = () => {
-    setShowFilter(!showFilter);
-    if (!showFilter) {
-      document.querySelector(".filter-box").style.borderRadius = "8px 8px 0 0";
-    } else {
-      document.querySelector(".filter-box").style.borderRadius = "8px";
-    }
-  };
-
-  const filterList = ["Price: Low to High", "Price: High to Low"];
-  const selectFilter = (filterType) => {
-    setCurrentPage(1);
-    setFilter(filterType);
-    toggleFilter();
-  };
-
-  document.body.addEventListener("click", (event) => {
-    const filterBox = document.querySelector(".filter-box");
-    if (filterBox && !event.target.closest(".filter-box")) {
-      setShowFilter(false);
-      filterBox.style.borderRadius = "8px";
-    }
-  });
-
-  const unFilter = () => {
-    setCurrentPage(1);
-    setFilter("");
-    toggleFilter();
-  };
+  const filterList = ["Below $1000", "$1000 to $2000", "Above $2000"];
 
   useEffect(() => {
     if (prevNameRef.current !== name) {
@@ -74,10 +46,12 @@ const SingleCategory = () => {
     let fetchDomain = "";
     if (filter === "") {
       fetchDomain = `category=${name}&page=${currentPage}`;
-    } else if (filter === "Price: Low to High") {
-      fetchDomain = `category=${name}&page=${currentPage}&sort=price`;
-    } else if (filter === "Price: High to Low") {
-      fetchDomain = `category=${name}&page=${currentPage}&sort=-price`;
+    } else if (filter === "Below $1000") {
+      fetchDomain = `category=${name}&page=${currentPage}&sort=price&filter=0,1000`;
+    } else if (filter === "$1000 to $2000") {
+      fetchDomain = `category=${name}&page=${currentPage}&sort=price&filter=1000,2000`;
+    } else if (filter === "Above $2000") {
+      fetchDomain = `category=${name}&page=${currentPage}&sort=price&filter=2000,100000`;
     }
     fetch(domain + fetchDomain)
       .then((res) => res.json())
@@ -88,7 +62,7 @@ const SingleCategory = () => {
       });
     window.scrollTo({
       top: 0,
-      behavior: "smooth", // Add smooth scrolling behavior
+      behavior: "smooth",
     });
   }, [currentPage, filter, name]);
 
@@ -112,23 +86,12 @@ const SingleCategory = () => {
         </div>
         <div className="home-section-banner">
           <div></div>
-          <div className="filter-box" onClick={toggleFilter}>
-            {filter ? filter : "Sort by"}
-            {filter ? (
-              <FontAwesomeIcon icon={faXmark} onClick={unFilter} />
-            ) : (
-              <FontAwesomeIcon icon={faAngleDown} />
-            )}
-          </div>
-          {showFilter && (
-            <div className="filter-dropdown">
-              {filterList.map((item, index) => (
-                <div key={index} onClick={() => selectFilter(item)}>
-                  {item}
-                </div>
-              ))}
-            </div>
-          )}
+          <Filter
+            filter={filter}
+            filterList={filterList}
+            setFilter={setFilter}
+            setCurrentPage={setCurrentPage}
+          />
         </div>
         {loadPage ? (
           <div className="single-loader-container">
