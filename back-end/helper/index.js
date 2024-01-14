785322b1a9d0854e471706cea39d3ab0aca83100
kietnguyen2003 <nguyengiakiet0987654321@gmail.com>
2024-01-14 18:32:28 +0700

    Back-end Succes statistic

--- back-end/helper/index.js
+++ back-end/helper/index.js
@@ -15,16 +15,21 @@ exports.sortObject = (obj) => {
 }
 
 exports.isDayEqual = (date1, date2) => {
-	const day1 = date1.getUTCDate();
-	const month1 = date1.getUTCMonth();
-	const year1 = date1.getUTCFullYear();
-  
-	const day2 = date2.getUTCDate();
-	const month2 = date2.getUTCMonth();
-	const year2 = date2.getUTCFullYear();
-  
-	return day1 === day2 && month1 === month2 && year1 === year2;
+    if (!(date1 instanceof Date) || !(date2 instanceof Date)) {
+        return false; // Hoặc thực hiện xử lý khác tùy thuộc vào yêu cầu của bạn
+    }
+
+    const day1 = date1.getUTCDate();
+    const month1 = date1.getUTCMonth();
+    const year1 = date1.getUTCFullYear();
+
+    const day2 = date2.getUTCDate();
+    const month2 = date2.getUTCMonth();
+    const year2 = date2.getUTCFullYear();
+
+    return day1 === day2 && month1 === month2 && year1 === year2;
 }
+
 
 exports.getDate = (startDay, endDay) => {
 	const result = [];
@@ -36,6 +41,28 @@ exports.getDate = (startDay, endDay) => {
 	}
   
 	return result;
+}
+
+exports.getWeeksInMonth = (year, month) => {
+    const firstDayOfMonth = new Date(year, month, 1);
+    const lastDayOfMonth = new Date(year, month + 1, 0);
+
+    const firstDayOfWeek = firstDayOfMonth.getUTCDay();
+
+    const daysInMonth = lastDayOfMonth.getUTCDate();
+    const weeks = Math.ceil((daysInMonth + firstDayOfWeek) / 7);
+	
+    return weeks;
+}
+
+exports.getStartOfWeek = (year, month, week) => {
+	const firstDayOfMonth = new Date(Date.UTC(year, month, 1));
+    const daysUntilFirstSunday = (7 - firstDayOfMonth.getUTCDay()) % 7;
+
+    const startOfWeek = new Date(firstDayOfMonth);
+    startOfWeek.setUTCDate(firstDayOfMonth.getUTCDate() + daysUntilFirstSunday + (week - 1) * 7);
+
+    return startOfWeek;
 }
 
 exports.getMonth = (year, monthIndex) => {
