297cc65746c0d09c97b43df8428b588346cbb74e
SimonGin <tt26419@gmail.com>
2024-01-14 08:38:32 +0700

    Front-end Update Category for Admin

--- front-end/src/pages/AdminProducts/AdminProducts.jsx
+++ front-end/src/pages/AdminProducts/AdminProducts.jsx
@@ -12,6 +12,7 @@ import {
   faXmark,
   faEye,
 } from "@fortawesome/free-solid-svg-icons";
+import Filter from "../../components/Filter/Filter";
 import Pagination from "../../components/Pagination/Pagination";
 import Loader from "../../components/Loader/Loader";
 import Toastify from "../../components/Toastify/Toastify";
@@ -30,25 +31,10 @@ const AdminProducts = () => {
   const [filter, setFilter] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
-  const [showFilter, setShowFilter] = useState(false);
   const [loadPage, setLoadPage] = useState(false);
   const [listChange, setListChange] = useState(false);
-  const toggleFilter = () => {
-    setShowFilter(!showFilter);
-    if (!showFilter) {
-      document.querySelector(".home-filter-box").style.borderRadius =
-        "8px 8px 0 0";
-    } else {
-      document.querySelector(".home-filter-box").style.borderRadius = "8px";
-    }
-  };
 
   const filterList = ["Below $1000", "$1000 to $2000", "Above $2000"];
-  const selectFilter = (filterType) => {
-    setCurrentPage(1);
-    setFilter(filterType);
-    toggleFilter();
-  };
 
   const prevFilterRef = useRef("all");
   const prevPage = useRef(1);
@@ -72,7 +58,7 @@ const AdminProducts = () => {
     } else if (filter === "$1000 to $2000") {
       fetchDomain = `page=${currentPage}&limit=12&sort=price&filter=1000,2000`;
     } else if (filter === "Above $2000") {
-      fetchDomain = `page=${currentPage}&limit=12&sort=price&filter=2000,10000`;
+      fetchDomain = `page=${currentPage}&limit=12&sort=price&filter=2000,100000`;
     }
     fetch(domain + fetchDomain)
       .then((res) => res.json())
@@ -82,20 +68,6 @@ const AdminProducts = () => {
         setLoadPage(false);
       });
   }, [currentPage, filter, listChange]);
-
-  document.body.addEventListener("click", (event) => {
-    const homeFilterBox = document.querySelector(".home-filter-box");
-    if (homeFilterBox && !event.target.closest(".home-filter-box")) {
-      setShowFilter(false);
-      homeFilterBox.style.borderRadius = "8px";
-    }
-  });
-
-  const unFilter = () => {
-    setCurrentPage(1);
-    setFilter("");
-    toggleFilter();
-  };
 
   const hideCategory = (name) => {
     fetch("http://localhost:8000/api/v1/category/hidden", {
@@ -116,6 +88,50 @@ const AdminProducts = () => {
           setListChange(!listChange);
         }
       });
+  };
+
+  const upgradeCategory = (name) => {
+    Swal.fire({
+      title: "Update Category",
+      input: "text",
+      inputPlaceholder: "Enter new category name",
+      showCancelButton: true,
+      confirmButtonText: "Submit",
+      cancelButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch("http://localhost:8000/api/v1/category/update", {
+          credentials: "include",
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+          },
+          body: JSON.stringify({
+            category: name,
+            newcategory: result.value,
+          }),
+        })
+          .then((res) => res.json())
+          .then((json) => {
+            console.log(json);
+            if (json.status === "success") {
+              setCategoryUpdate(!categoryUpdate);
+              Swal.fire({
+                title: "Category Updated!",
+                text: `Successfully updated category name from "${name}" to "${result.value}"`,
+                icon: "success",
+              });
+            } else {
+              Swal.fire({
+                title: "Error while updating!",
+                text: "There's some error!!! Please try again later",
+                icon: "error",
+              });
+            }
+          });
+      }
+    });
   };
 
   const addCategory = () => {
@@ -198,42 +214,52 @@ const AdminProducts = () => {
             <FontAwesomeIcon icon={faTags} /> Categories
           </h2>
         </div>
-        <div className="home-section-content">
-          <div className="categories-list">
+        <div className="admin-products-section-content">
+          <div className="admin-products-categories-list">
             {categoryList.map((category, idx) => (
-              <div
-                key={idx}
-                className="category-card"
-                onClick={() => hideCategory(category.name)}
-                onMouseEnter={() => setHoveredIndex(idx)}
-                onMouseLeave={() => setHoveredIndex(null)}
-              >
-                <div className="category-card-bg"></div>
-                <h3 className="category-card-name">
-                  {category.isHidden ? (
-                    <>
-                      {hoveredIndex === idx ? (
-                        <FontAwesomeIcon icon={faEye} />
-                      ) : (
-                        <>
-                          <FontAwesomeIcon
-                            icon={faEyeSlash}
-                            className="admin-hidden-category"
-                          />
-                          {category.name}
-                        </>
-                      )}
-                    </>
-                  ) : (
-                    <>
-                      {hoveredIndex === idx ? (
-                        <FontAwesomeIcon icon={faEyeSlash} />
-                      ) : (
-                        category.name
-                      )}
-                    </>
-                  )}
-                </h3>
+              <div className="admin-products-category-container">
+                <div
+                  key={idx}
+                  className="category-card"
+                  onClick={() => hideCategory(category.name)}
+                  onMouseEnter={() => setHoveredIndex(idx)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                >
+                  <div className="category-card-bg"></div>
+                  <h3 className="category-card-name">
+                    {category.isHidden ? (
+                      <>
+                        {hoveredIndex === idx ? (
+                          <FontAwesomeIcon icon={faEye} />
+                        ) : (
+                          <>
+                            <FontAwesomeIcon
+                              icon={faEyeSlash}
+                              className="admin-hidden-category"
+                            />
+                            {category.name}
+                          </>
+                        )}
+                      </>
+                    ) : (
+                      <>
+                        {hoveredIndex === idx ? (
+                          <FontAwesomeIcon icon={faEyeSlash} />
+                        ) : (
+                          category.name
+                        )}
+                      </>
+                    )}
+                  </h3>
+                </div>
+                <div className="category-card-upgrade-container">
+                  <div
+                    className="category-card-upgrade-btn"
+                    onClick={() => upgradeCategory(category.name)}
+                  >
+                    <FontAwesomeIcon icon={faCircleUp} />
+                  </div>
+                </div>
               </div>
             ))}
           </div>
@@ -257,23 +283,13 @@ const AdminProducts = () => {
           <h2>
             <FontAwesomeIcon icon={faStore} /> Products
           </h2>
-          <div className="home-filter-box" onClick={toggleFilter}>
-            {filter ? filter : "Filter"}
-            {filter ? (
-              <FontAwesomeIcon icon={faXmark} onClick={unFilter} />
-            ) : (
-              <FontAwesomeIcon icon={faAngleDown} />
-            )}
-          </div>
-          {showFilter && (
-            <div className="filter-dropdown">
-              {filterList.map((item, index) => (
-                <div key={index} onClick={() => selectFilter(item)}>
-                  {item}
-                </div>
-              ))}
-            </div>
-          )}
+          <Filter
+            filterName={"Price"}
+            filter={filter}
+            filterList={filterList}
+            setFilter={setFilter}
+            setCurrentPage={setCurrentPage}
+          />
         </div>
         <div className="admin-products">
           {loadPage ? (
